b6114880ed80f985b068a0be42b78de871730094
magda <magda@DESKTOP-L6E1CFA>
2019-11-10 20:24:07 +0100

    Retoques home

--- proyecto/src/main/java/com/example/proyecto/HomeController.java
+++ proyecto/src/main/java/com/example/proyecto/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.proyecto;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,16 @@ public class HomeController {
 	@RequestMapping("/megapop")
 	public String home() {
 		return "home/index.html";
+
 	}
 
+	@GetMapping("register")
+	public String userRegister() {
+		return "register/userRegister";
+	}
 	
+	@GetMapping("home")
+	public String index() {
+		return "home/index.html";
+	}
 }
