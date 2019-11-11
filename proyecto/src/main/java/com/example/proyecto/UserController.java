320fb192f78bcd9eef91ffa76208bf451ecc060e
magda <magda@DESKTOP-L6E1CFA>
2019-11-11 17:35:56 +0100

    Miratelo Marc

--- proyecto/src/main/java/com/example/proyecto/UserController.java
+++ proyecto/src/main/java/com/example/proyecto/UserController.java
@@ -1,13 +1,24 @@
 package com.example.proyecto;
 
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 
-	public class UserController {
+	public class UserController{
 	
 	@Autowired
 	UserService service;
@@ -26,5 +37,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 		return "home/profile";
 	}
 
+	@GetMapping("/home/login")
+	public String showLoginForm (HttpServletRequest request, HttpServletResponse response, Model model) {
+		return "/home/login";
+	}
+	
+	@PostMapping ("/home/login")
+	public String loginProcess (HttpServletRequest request, HttpServletResponse response, @ModelAttribute("login") LoginBean login, HttpSession session) {
 		
-}
+		User user = UserService.findbyUser(login.getEmail());
+		
+		if (null != login && user != null && user.getPassword().equals(login.getPassword())) {
+			session.setAttribute("user", user);
+		} else {
+			session.setAttribute("error_userAuthentification", "Username or password is wrong!");
+			return "user/login";
+		}
+		return "redirect:/";
+	}
+	
+	
+	}
--- proyecto/src/main/java/com/example/proyecto/UserService.java
+++ proyecto/src/main/java/com/example/proyecto/UserService.java
@@ -1,20 +1,24 @@
 package com.example.proyecto;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService {
+public class UserService{
 
 	@Autowired
+	static	
 	UserRepository repository;
 	
 	
-	public Iterable<User> findAll() {
+	public List<User> findAll() {
 		
-		return repository.findAll();
+		return (List<User>)repository.findAll();
 	}
 	
 	public void insertUser(User user) {
@@ -22,7 +26,8 @@ public class UserService {
 		repository.save(user);
 	}
 	
-	
-
+	public static User findbyUser(String user) {
+		return repository.findById(user).orElse(null);
+	}
 
 }
